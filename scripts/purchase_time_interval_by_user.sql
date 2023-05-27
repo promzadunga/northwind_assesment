8f5bcad73ab8a0f2ae12bb3601a27c4b68ddca72
promzadunga <37542040+promzadunga@users.noreply.github.com>
2023-05-27 13:29:09 +0200

    Update purchase_time_interval_by_user.sql

--- scripts/purchase_time_interval_by_user.sql
+++ scripts/purchase_time_interval_by_user.sql
@@ -1,1 +1,5 @@
-
+SELECT customer_id, TIMESTAMPDIFF(DAY, MIN(order_date), MAX(order_date)) AS purchase_interval
+FROM orders
+GROUP BY customer_id
+HAVING COUNT(*) > 1
+ORDER BY purchase_interval DESC;
